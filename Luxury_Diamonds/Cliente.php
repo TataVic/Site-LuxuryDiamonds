9471f1219520ed69e519f0d67dc0b0047d6a0a0e
Tauani Vitória <tauanivitoria1302@gmail.com>
2023-11-23 17:03:11 -0300

    Adição de todos os arquivos que estou atualmente configurando

---
+++ Luxury_Diamonds/Cliente.php
@@ -0,0 +1,81 @@
+<?php
+class Cliente extends model{
+
+	public function adicionar($nome,$data_nascimento, $cpf,$endereco, $sexo, $email, $senha){
+		$sql = "INSERT INTO pessoa (nome, data_nascimento, cpf, endereco, sexo, email, senha)
+		        VALUES (:nome, :data_nascimento, :cpf, :endereco, :sexo, :email, :senha)";
+
+		$sql = $this->db->prepare($sql);
+		$sql->bindValue(":nome"    , $nome);
+		$sql->bindValue(":data_nascimento"   , $data_nascimento);
+        $sql->bindValue(":cpf"    , $cpf);
+        $sql->bindValue(":endereco", $endereco);
+        $sql->bindValue(":sexo"    , $sexo);
+        $sql->bindValue(":email"    , $email);
+        $sql->bindValue(":senha"    , $senha);
+		$sql->execute();
+
+		return $this->db->lastInsertId();
+	}
+
+	public function editar($nome,$data_nascimento, $cpf,$endereco, $sexo, $email, $senha){
+		$sql = "UPDATE pessoa 
+		           SET nome     = :nome
+		             , data_nascimento    = :data_nascimento
+                     , cpf      = :cpf
+                     , endereco = :endereco
+		             , sexo     = :sexo
+                     , email    = :email
+                     , senha    = :senha
+		         WHERE id_pessoa = :id_pessoa";
+
+		$sql = $this->db->prepare($sql);
+		$sql->bindValue(":nome"     , $nome);
+		$sql->bindValue(":data_nascimento"    , $data_nascimento);
+        $sql->bindValue(":cpf" , $cpf);
+        $sql->bindValue(":endereco" , $endereco);
+		$sql->bindValue(":sexo"     , $sexo);
+        $sql->bindValue(":email" , $email);
+        $sql->bindValue(":senha" , $senha);
+		$sql->bindValue(":id_pessoa", $id_pessoa);
+		$sql->execute();
+	}
+
+	public function excluir($id_pessoa){
+		$sql = "DELETE FROM pessoa WHERE id_pessoa = :id_pessoa";
+
+		$sql = $this->db->prepare($sql);
+		$sql->bindValue(":id_pessoa", $id_pessoa);
+		$sql->execute();
+	}
+
+	public function get($id_pessoa){
+		$array = array();
+
+		$sql = "SELECT * 
+		          FROM pessoa
+		         WHERE id_pessoa = :id_pessoa";
+		$sql = $this->db->prepare($sql);
+		$sql->bindValue(':id_pessoa', $id_pessoa);
+		$sql->execute();
+
+		if($sql->rowCount() > 0){
+			$array = $sql->fetch(\PDO::FETCH_ASSOC);
+		}		
+
+		return $array;
+	}
+
+	public function getAll(){
+		$array = array();
+
+		$sql = "SELECT * FROM PESSOA ORDER BY NOME";
+		$sql = $this->db->query($sql);
+
+		if($sql->rowCount() > 0){
+			$array = $sql->fetchAll(\PDO::FETCH_ASSOC);
+		}		
+
+		return $array;
+	}
+}
