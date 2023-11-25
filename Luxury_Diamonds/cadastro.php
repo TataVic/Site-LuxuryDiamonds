c6ce61bdd864bd31bb71b55e3596b41e7bca008a
Tauani Vitória <tauanivitoria1302@gmail.com>
2023-11-24 21:45:06 -0300

    Mudanças, e estará pausado o projeto

---
+++ Luxury_Diamonds/cadastro.php
@@ -0,0 +1,47 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<body>
+<div class="container">
+        <div class="content first-content">
+            <div class="firs-column">
+                <h2 class="title">Bem-Vindo, ao Jewellery Luxury Diamonds!</h2>
+                <p class="description">kkkkkkkkkkkkkkk</p>
+            </div>
+            <div class="second-column">
+                <h2 class="title">Crie sua conta:</h2>
+                <p class="description">preencha os campos:</p>
+                <form>
+                    <label>Nome:</label>
+                    <input type="text" placeholder="Nome">
+                    <label>Data de Nascimento:</label>
+                    <input type="date">
+                    <label>CPF:</label>
+                    <input type="text">
+                    <label>Telefone:</label>
+                    <input type="text">
+                    <label>Rua:</label>
+                    <input type="text">
+                    <label>Numero:</label>
+                    <input type="text">
+                    <label>CEP:</label>
+                    <input type="text">
+                    <label>Complemento:</label>
+                    <input type="text">
+                    <label>Cidade:</label>
+                    <input type="text">
+                    <button class="button">Salvar</button>
+                </form>
+            </div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
